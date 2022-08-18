560e5b2b9916a32f737b2a1561f713f967df296a
Pablo Daniel Villagrán <pdvillc@gmail.com>
2022-08-18 15:40:03 -0600

    readable routes

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,7 @@ const tokenator = require('../auth/authenticateToken')//modulo de manejo de toke
 
 require('../auth/authenticateUser')//carga de configuraciones de passport
 
-router.post('/0000/client', passport.authenticate('signup', {session: false}), async (request, response, next)=>{//create user
+router.post('/signup', passport.authenticate('signup', {session: false}), async (request, response, next)=>{//create user
     const user = {_id:request.user._id, email:request.user.email}
     const token = tokenator.generateAccessToken(user);//tokeniza usuario y _id
     const refresh = tokenator.generateRefreshToken(user);//genera llave "refresh" para usuario
@@ -19,7 +19,7 @@ router.post('/0000/client', passport.authenticate('signup', {session: false}), a
     });
 })
 
-router.post('/0001/client', passport.authenticate('login', {session:false}), async (request, response, next)=>{//login user
+router.post('/login', passport.authenticate('login', {session:false}), async (request, response, next)=>{//login user
     const user = {_id:request.user._id, email:request.user.email}
     const token = tokenator.generateAccessToken(user);//tokeniza usuario y _id
     const refresh = tokenator.generateRefreshToken(user);//genera llave "refresh" para usuario
@@ -30,11 +30,11 @@ router.post('/0001/client', passport.authenticate('login', {session:false}), asy
     });
 })
 
-router.delete('/0001/client', tokenator.deleteRefresh, (request, response)=>{//ruta de logout
+router.delete('/login', tokenator.deleteRefresh, (request, response)=>{//ruta de logout
     console.log('logout')
 })
 
-router.post('/0001/client/refresh', tokenator.validateRefresh, (request, response)=>{//ruta de refresh
+router.post('/login/refresh', tokenator.validateRefresh, (request, response)=>{//ruta de refresh
     console.log('refresh token')
 })
 
